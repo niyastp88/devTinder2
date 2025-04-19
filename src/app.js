b694abb0a3fc2dd10f973964756d4d4f0dba926d
niyas tp <niyastp2000@gmail.com>
2025-04-19 13:32:12 +0530

    added dynamic data to database

--- src/app.js
+++ src/app.js
@@ -2,14 +2,10 @@ const express=require('express')
 const connectDB=require('./config/database')
 const User=require('./models/user')
 const app=express()
+app.use(express.json())
 
 app.post('/signup',async (req,res)=>{
-    const user=new User({
-        firstName:"muhammed",
-        lastName:"niyas",
-        emailId:"niyastp2000@gmail.com",
-        password:"niyas@123"
-    })
+    const user=new User(req.body)
     await user.save()
 
     res.send("user added susscessfully")
@@ -21,7 +17,7 @@ connectDB().then(()=>{
         console.log("server running")
     })
 }).catch((err)=>{
-    console.error('database cannot be conncted')
+    console.error(err.message)
 })
 
 
