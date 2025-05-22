c8d8c0f29f4de4258265e3230769be334fbf8c8d
niyas tp <niyastp2000@gmail.com>
2025-05-22 10:06:49 +0530

    schema validations

--- src/app.js
+++ src/app.js
@@ -4,11 +4,42 @@ const User=require('./models/user')
 const app=express()
 app.use(express.json())
 
+app.get("/",(req,res)=>{
+    res.send("hello world")
+})
+
 app.post('/signup',async (req,res)=>{
+    console.log(req.body)
     const user=new User(req.body)
     await user.save()
 
     res.send("user added susscessfully")
+})
+app.get('/user',async (req,res)=>{
+    const userEmail=req.body.emailId
+    try{
+        const user=await User.find({emailId:userEmail})
+        if(user.length===0){
+            res.status(404).send("user not found")
+        }else{
+           res.send(user)
+        }
+        
+    }
+    catch(err){
+        res.status(400).send("something went wrong")
+        console.log("something went wrong")
+    }
+})
+
+app.get("/feed",async (req,res)=>{
+    try{
+        const users=await User.find({})
+        res.send(users)
+    }catch(err){
+        res.send(err.message)
+    }
+
 })
 
 connectDB().then(()=>{
--- src/models/user.js
+++ src/models/user.js
@@ -2,23 +2,50 @@ const mongoose=require('mongoose')
 
 const userSchema=mongoose.Schema({
     firstName:{
-        type:String
+        type:String,
+        required:true,
+        minLength:4,
+        maxLength:50
     },
     lastName:{
         type:String
     },
     emailId:{
-        type:String
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true,
     },
     password:{
-        type:String
+        type:String,
+        required:true,
     },
     age:{
-        type:String
+        type:Number,
+        min:18
     },
     gender:{
-        type:String
+        type:String,
+        validate(value){
+            if(!["male","female","others"].includes(value)){
+                throw new Error("Gender data is not valid")
+            }
+        }
+    },
+    photoUrl:{
+        type:String,
+        default:"https://www.pnrao.com/wp-content/uploads/2023/06/dummy-user-male.jpg"
+    },
+    about:{
+        type:String,
+        default:"This is the default about of the user"
+    },
+    skills:{
+        type:[String]
     }
+},{
+    timestamps:true,
 })
 
 
